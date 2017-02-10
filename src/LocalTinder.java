4ae7cb14db9260b5535370864f807f7cf50faac2
tinaditte <tinaditte@users.noreply.github.com>
2017-02-10 10:58:25 +0100

    IC

---
+++ src/LocalTinder.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * Created by tinab on 10-Feb-17.
+ */
+public class LocalTinder extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        showImage(primaryStage, "file:C:/Users/tinab/OneDrive/Pictures/Skærmbilleder/yo.png");
+    }
+
+    public void showImage (Stage stage, String url) {
+        ImageView imageView = new ImageView(url);
+        imageView.setFitHeight(500);
+        imageView.setPreserveRatio(true);
+        VBox vBox = new VBox(imageView);
+        vBox.setPadding(new Insets(30, 30, 30, 30));
+        vBox.setStyle("-fx-background-color: black");
+        Scene scene = new Scene(vBox);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
